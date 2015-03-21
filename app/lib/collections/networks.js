b3b6c441e48052223a318605e05f4742c3dc861b
Joe Burdick <josephdburdick@gmail.com>
2015-03-21 02:09:57 -0400

    Initial commit.

---
+++ app/lib/collections/networks.js
@@ -0,0 +1,32 @@
+Networks = new Mongo.Collection('networks');
+
+
+if (Meteor.isServer) {
+  Networks.allow({
+    insert: function (userId, doc) {
+      return false;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return false;
+    },
+
+    remove: function (userId, doc) {
+      return false;
+    }
+  });
+
+  Networks.deny({
+    insert: function (userId, doc) {
+      return true;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return true;
+    },
+
+    remove: function (userId, doc) {
+      return true;
+    }
+  });
+}
