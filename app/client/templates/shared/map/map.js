59effc8f73756666c5dafc8887f9336b49b38b95
Joe Burdick <josephdburdick@gmail.com>
2015-03-26 12:52:01 -0400

    Properly show individual lat/lng for each item. Refresh map when switching between items and properly center the map when window resizing.

--- app/client/templates/shared/map/map.js
+++ app/client/templates/shared/map/map.js
@@ -12,11 +12,10 @@ Template.Map.helpers({
     // Make sure the maps API has loaded
     if (GoogleMaps.loaded()) {
       // Map initialization options
-      var self = this;
-
+      var self = Template.instance().data;
       return {
         center: new google.maps.LatLng(
-        	this.latitude, this.longitude
+        	self.latitude, self.longitude
         ),
         zoom: 15
       };
@@ -28,33 +27,38 @@ Template.Map.helpers({
 /* Map: Lifecycle Hooks */
 /*****************************************************************************/
 var thisMap = {
-	init: function(){
+	init: function(latitude, longitude){
 		var self = this;
 		GoogleMaps.ready('networkItemMap', function(map) {
+
+			map.options.center.k = latitude;
+			map.options.center.D = longitude;
+
 	    var marker = new google.maps.Marker({
 	      position: map.options.center,
 	      map: map.instance
 	    });
-	    Session.set('center', map.options.center);
-	    var instance = map.instance;
-				
+
+	    var mapInstance = map.instance;
+			
 			function calculateCenter(){
-				if (instance){
-					Session.set('center', instance.getCenter());
+				if (mapInstance){
+					debugger;
+					Session.set('center', mapInstance.getCenter());
+					debugger;
 				}
 			}
-			
 			self.center(GoogleMaps.maps.networkItemMap);
 	  });
 	}, 
 	center: function(map){
 		$(window).resize(function() {
-		  map.instance.setCenter(
-		  	new google.maps.LatLng(
-		  		map.options.center.k,
-					map.options.center.D
-				)
-	  	);
+			// debugger;
+			var centerLatLng = new google.maps.LatLng(
+	  		Session.get('latitude'),
+				Session.get('longitude')
+			);
+		  map.instance.setCenter(centerLatLng);
 	  	console.log('=> triggered resize');
 		}).trigger('resize');
 	}
@@ -64,10 +68,16 @@ Template.Map.created = function () {
 };
 
 Template.Map.rendered = function () {
-	thisMap.init();
+	var self = Template.instance().data;
+	Session.set('latitude', self.latitude);
+	Session.set('longitude', self.longitude);
+	
+	thisMap.init(Session.get('latitude'), Session.get('longitude'));
 	
 	$('body').scrollTop($('nav').outerHeight());
 };
 
 Template.Map.destroyed = function () {
+	Session.set('latitude', null);
+	Session.set('longitude', null);
 };
