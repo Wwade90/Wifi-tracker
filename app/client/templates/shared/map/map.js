497191ba0d87ea7a1c9c532e3616c1a9c236ba12
Joe Burdick <josephdburdick@gmail.com>
2015-04-12 21:45:49 -0400

    Add map functionality to show lat/long of template instance data or 0,0 if no data available.

--- app/client/templates/shared/map/map.js
+++ app/client/templates/shared/map/map.js
@@ -8,62 +8,42 @@ Template.Map.events({
 /* Map: Helpers */
 /*****************************************************************************/
 Template.Map.helpers({
-	log: function () {
-    console.log(this);
-  },
-	networkItemMapOptions: function() {
+	networkMapOptions: function() {
     // Make sure the maps API has loaded
     if (GoogleMaps.loaded()) {
+      
       // Map initialization options
-      var self = Template.instance().data;
-      if (self == null){
-      	self = {
-      		latitude: Session.get('latitude'),
-      		longitude: Session.get('longitude')
-      	};
-      }
+      var data = Template.instance().data;
+      if (!data)
+      	data = { latitude: 0, longitude: 0 };
+      
       return {
         center: new google.maps.LatLng(
-        	self.latitude, self.longitude
+        	data.latitude, data.longitude
         ),
        	scrollwheel: false,
         zoom: 17
       };
     }
   }
+
 });
 
 /*****************************************************************************/
 /* Map: Lifecycle Hooks */
 /*****************************************************************************/
 var thisMap = {
-	init: function(latitude, longitude){
+	init: function(ll){
 		var self = this;
-		GoogleMaps.ready('networkItemMap', function(map) {
 
-			map.options.center.k = latitude;
-			map.options.center.D = longitude;
-
+		GoogleMaps.ready('networkMap', function(map) {
 	    var marker = new google.maps.Marker({
 	      position: map.options.center,
 	      animation: google.maps.Animation.DROP,
 	      map: map.instance
-	    });
+	    });	
 
-	    var mapInstance = map.instance;
-			
-			console.log('map starting?');
-			$(window).resize(function(e) {
-				var centerLatLng = new google.maps.LatLng(
-		  		Session.get('latitude'),
-					Session.get('longitude')
-				);
-			  map.instance.setCenter(centerLatLng);
-		  	console.log('=> triggered resize');
-			});
-			self.center(GoogleMaps.maps.networkItemMap);
 	  });
-	  
 	}, 
 	center: function(){
 		$(window).trigger('resize');
@@ -71,19 +51,25 @@ var thisMap = {
 };
 
 Template.Map.created = function () {
+	thisMap.init(Session.get('ll'));
+	GoogleMaps.ready('networkMap', function(map) {
+		var center;
+		function calculateCenter() {
+		  center = map.instance.getCenter();
+		}
+		google.maps.event.addListener(map.instance, 'idle', function() {
+		  calculateCenter();
+		});
+	
+		$(window).resize(function(){
+			map.instance.setCenter(center);
+		});
+
+  });
 };
 
 Template.Map.rendered = function () {
-	if (Template.instance().data !== null){
-		Session.set('latitude', Template.instance().data.latitude);
-		Session.set('longitude', Template.instance().data.longitude);
-		thisMap.init(Session.get('latitude'), Session.get('longitude'));	
-	}
-	
-	$('body').scrollTop($('nav').outerHeight());
 };
 
 Template.Map.destroyed = function () {
-	Session.set('latitude', null);
-	Session.set('longitude', null);
 };
