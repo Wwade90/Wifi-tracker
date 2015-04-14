3dacff687fa7d44712e7fc9894fd43200b8a8282
Joe Burdick <josephdburdick@gmail.com>
2015-04-14 00:44:05 -0400

    Apply geocode object to data-location within location field; submit form; begin converting dummy data to reflect data within the db.

--- app/client/templates/networks/network_create/network_create.js
+++ app/client/templates/networks/network_create/network_create.js
@@ -8,9 +8,19 @@ Template.NetworkCreate.events({
 			console.log("Approximate street address is: " + Session.get('streetAddress'));
 		}, 5000);
 	},
-	'submit #network_create': function(e){
+	'submit #network_create': function(e, tmpl){
 		e.preventDefault();
-		console.log('Submitting form...');
+		var network = {
+			userId: Meteor.userId(),
+			createdAt: new Date,
+			network_name: tmpl.find('#network_name').val,
+			network_password: tmpl.find('#network_password').val,
+			location_name: tmpl.find('#location_name').val,
+			location_address: Session.get('locationData'),
+			isPublic: tmpl.find('#network_isPublic').checked
+		}
+		Networks.insert(network);
+		tmpl.find('form').reset();
 	}
 });
 
@@ -20,6 +30,9 @@ Template.NetworkCreate.events({
 Template.NetworkCreate.helpers({
 	streetAddress: function(){
 		return Session.get('streetAddress');
+	},
+	locationData: function(){
+		return Session.get('locationData');
 	}
 });
 
@@ -28,25 +41,36 @@ Template.NetworkCreate.helpers({
 /*****************************************************************************/
 var getGeolocation = function(){
 	if (Meteor.isClient) {
+		var locationData = 0;
 		navigator.geolocation.getCurrentPosition(function(position) {
       Session.set('lat', position.coords.latitude);
       Session.set('lon', position.coords.longitude);
-
-      var address = Meteor.call('reverseGeocode', [Session.get('lat'), Session.get('lon')], 
-      	function(error, result){
-			    if(error){
-			        console.log(error.reason);
-			    } else {
-			      Session.set('streetAddress', (result[0].streetNumber + " " + result[0].streetName))
-			    }
-			  }
-			);		        
+      
+      var address = Meteor.call('reverseGeocode', [Session.get('lat'), Session.get('lon')], function(error, result){
+		    if (error){
+		    	return alert(error.reason);
+		    } else {
+		      Session.set('streetAddress', (result[0].streetNumber + " " + result[0].streetName))
+		      locationData = {
+		      	latitude: result[0].latitude,
+		      	longitude: result[0].longitude,
+		      	streetName: result[0].streetName,
+		      	city: result[0].city,
+		      	zipcode: result[0].zipcode,
+		      	state: result[0].state,
+		      	stateCode: result[0].stateCode,
+	      		country: result[0].country,
+						countryCode: result[0].countryCode
+		      };
+		      Session.set('locationData', locationData);
+		    }
+			});
     });
   }
 };
 
 Template.NetworkCreate.created = function () {
-	getGeolocation();	
+	getGeolocation();
 };
 
 Template.NetworkCreate.rendered = function () {
