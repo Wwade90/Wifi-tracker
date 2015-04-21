b1c0f15a18818e43ae39176865796b0f60a1b291
Joe Burdick <josephdburdick@gmail.com>
2015-04-21 01:26:03 -0400

    Seed data base with Open NYC hotspot data.

--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -2,5 +2,15 @@ if (Meteor.isServer){
 	Meteor.startup(function(){
 	  console.log(Networks.find().count() + ' networks');
 	  console.log(Meteor.users.find().count() + ' users');
+
+	  // Seed Venues database
+	  if (Venues.find().count() === 0){
+	  	var venues = JSON.parse(Assets.getText('seed_venues.json'));
+		  _.each(venues, function(venue) {
+		    // replace this with something like Companions.insert(companion);
+		    var result = Meteor.call('addVenue', venue);
+		  });
+	  }
+		
 	});
 }
