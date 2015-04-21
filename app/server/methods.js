de49150935828e464c6527a10eddbe6d2c57a266
Joe Burdick <josephdburdick@gmail.com>
2015-04-21 01:26:57 -0400

    Simplify Foursquare get method.

--- app/server/methods.js
+++ app/server/methods.js
@@ -2,48 +2,19 @@
 /* Server Only Methods */
 /*****************************************************************************/
 Meteor.methods({
-	foursquare: function(){
+	foursquare: function(params){
 		var urlPrefix = 'https://api.foursquare.com/v2/venues/';
 		var timeStamp = (function(){ return (new Date()).toISOString().slice(0,10).replace(/-/g,"");})();
 		var auth = (function(){ return '&client_id='+ Meteor.settings.apiServices.foursquare.appId +'&client_secret='+ Meteor.settings.apiServices.foursquare.secret +'&v='})();
-		
-		var query = function(params){
-			return {
-				intent: params[0],
-				formatted: params[0] + '?' + params[1],
-				params: {
-					key: params[1].split('=')[0],
-					value: params[1].split('=')[1]
-				}
-			};
-		}
-		var url = function(){
-			var params = [];
-			for (var i=0; i < arguments.length; i++) {
-	      params.push(arguments[i]);
-	    }
-			var string = urlPrefix + query(params) + auth() + timeStamp;
-			console.log(string);
-			return string;
-		};
 
 		var get = function(){
-			var params = [];
-			for (var i=0; i < arguments.length; i++) {
-	      params.push(arguments[i]);
-	    }
-	    console.log('Params: ', params);
-	    console.log('Getting: ', urlPrefix + query(params).formatted + auth + timeStamp);
-	    var response = HTTP.get(urlPrefix + query(params).formatted + auth + timeStamp);
-
-	    console.log(response);
+	    console.log('Getting: ', urlPrefix + params + auth + timeStamp);
+	    var response = HTTP.get(urlPrefix + params + auth + timeStamp);
 	    return response;
 		};
 
 		return {
-			url : url,
-			get: get,
-			query: query
+			get: get
 		}
 	},
 	insertNetwork: function(network){
@@ -60,12 +31,22 @@ Meteor.methods({
 		var result = geo.reverse(latLng[0], latLng[1]);
 		return result;
 	},
+	addVenue: function(venue){
+		if (Meteor.user())
+			venue.userId = Meteor.userId();
+		venue.createdAt = new Date;
+		var newVenue = Venues.insert(venue);
+		return newVenue;
+	},
 	getVenues: function(latLng){
 		console.log("Fetching venues for [" + latLng +"]");
-		Meteor.call('foursquare').get('search', 'll=' + latLng.toString(), function(e, r){
-			if (!e)
+		var coords = latLng;
+		Meteor.call('foursquare', 'search?ll=' + coords, function(e, r){
+			if (!e){
+				// Session.set('localVenues', r);
 				return r;
-			else {
+			}
+			else{
 				return e;
 			}
 		});
