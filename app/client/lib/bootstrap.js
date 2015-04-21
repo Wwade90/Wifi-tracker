65832362dc8373ba2f477fd640de5c71b8f63a84
Joe Burdick <josephdburdick@gmail.com>
2015-04-20 21:14:30 -0400

    Use settings.json.

--- app/client/lib/bootstrap.js
+++ app/client/lib/bootstrap.js
@@ -1,3 +1,7 @@
+Accounts.ui.config({
+  passwordSignupFields: Meteor.settings.public.Accounts.config.passwordSignupFields
+});
+
 if (Meteor.isClient) {
 	Meteor.startup(function() {
 	  GoogleMaps.load();
