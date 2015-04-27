9ac259e81f5b15c224fa2c33674148bdae9875db
Joe Burdick <josephdburdick@gmail.com>
2015-04-27 08:26:53 -0400

    Clean up maps.

--- app/client/templates/shared/map/map.js
+++ app/client/templates/shared/map/map.js
@@ -13,8 +13,8 @@ Template.Map.helpers({
 	singleMarkerOptions: function() {
     // Make sure the maps API has loaded
     if (GoogleMaps.loaded()) {
-      
       // Map initialization options
+
       var data = Template.instance().data;
       if (!data){
       	data = { 
@@ -26,16 +26,11 @@ Template.Map.helpers({
       }
       return {
         center: new google.maps.LatLng(
-        	data.location.latitude, data.location.longitude
+        	data.location.lat, data.location.lon
         ),
        	scrollwheel: false,
         zoom: 19
       };
-    }
-  },
-  allVenues: function(){
-    if (GoogleMaps.loaded()) {
-
     }
   }
 
@@ -45,6 +40,7 @@ Template.Map.helpers({
 /* Map: Lifecycle Hooks */
 /*****************************************************************************/
 Template.Map.created = function () {
+  getUserGeolocation();
   GoogleMaps.ready('map', function(map) {
 		var marker = new google.maps.Marker({
       position: map.options.center,
@@ -59,23 +55,6 @@ Template.Map.created = function () {
 		$(window).resize(function(){
 			map.instance.setCenter(center);
 		});
-
-  });
-
-  GoogleMaps.ready('allMarkers', function(map) {
-    var marker = new google.maps.Marker({
-      position: map.options.center,
-      animation: google.maps.Animation.DROP,
-      map: map.instance
-    }); 
-    var center;
-    function calculateCenter() {
-      center = map.instance.getCenter();
-    }
-    
-    $(window).resize(function(){
-      map.instance.setCenter(center);
-    });
 
   });
 }
