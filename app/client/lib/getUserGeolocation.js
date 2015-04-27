bd10e61b1539839124bdae8e2772aa26316c87b8
Joe Burdick <josephdburdick@gmail.com>
2015-04-27 08:21:38 -0400

    Comment out geocode method from getUserGeolocation(), called too often and hit limit.

--- app/client/lib/getUserGeolocation.js
+++ app/client/lib/getUserGeolocation.js
@@ -1,3 +1,13 @@
+/*
+
+getUserGeolocation
+
+1. Grabs current position
+2. Sets lat, lon, currentUserCoords
+3. Calls reverseGeocode server method
+
+*/
+
 getUserGeolocation = function(){
 	if (Meteor.isClient) {
 		var locationData = 0,
@@ -5,30 +15,36 @@ getUserGeolocation = function(){
 		navigator.geolocation.getCurrentPosition(function(position) {
       Session.set('lat', position.coords.latitude);
       Session.set('lon', position.coords.longitude);
-      
-      Meteor.call('reverseGeocode', [Session.get('lat'), Session.get('lon')], function(error, result){
-		    if (error){
-		    	console.log(error.reason);
-		    	return false;
-		    } else {
-		      Session.set('streetAddress', (result[0].streetNumber + " " + result[0].streetName))
-		      Session.set('userGeolocation', {
-		      	lat: result[0].latitude,
-		      	lon: result[0].longitude,
-		      	streetName: result[0].streetName,
-		      	streetAddress: result[0].streetNumber + " " + result[0].streetName,
-		      	city: result[0].city,
-		      	zipcode: result[0].zipcode,
-		      	state: result[0].state,
-		      	stateCode: result[0].stateCode,
-	      		country: result[0].country,
-						countryCode: result[0].countryCode
-		      });
+      Session.set('currentUserCoords', [position.coords.latitude, position.coords.longitude]);
+     	// if (error){
+		    // 	console.log(error.reason);
+		    // 	return false;
+		    // } else {
+		    //   Session.set('streetAddress', (result[0].streetNumber + " " + result[0].streetName))
+		    //   Session.set('userGeolocation', {
+		    //   	userId: Meteor.userId(),
+		    //   	lat: result[0].latitude,
+		    //   	lon: result[0].longitude,
+		    //   	coords: [result[0].latitude, result[0].longitude],
+		    //   	streetName: result[0].streetName,
+		    //   	streetAddress: result[0].streetNumber + " " + result[0].streetName,
+		    //   	city: result[0].city,
+		    //   	zipcode: result[0].zipcode,
+		    //   	state: result[0].state,
+		    //   	stateCode: result[0].stateCode,
+	     //  		country: result[0].country,
+						// countryCode: result[0].countryCode
+		    //   });
 
-		      return result;
-		    }
-			});
+		    //   return result;
+		    // }
 			return position.coords;
+    },
+    function() {
+        alert('Position could not be determined.')
+    },
+    {
+        enableHighAccuracy: true
     });
   }
 };
