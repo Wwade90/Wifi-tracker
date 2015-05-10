002baa0ff8415b0d99318b012c90098ba6a13d51
Joe Burdick <josephdburdick@gmail.com>
2015-05-09 23:25:25 -0400

    Remove settings here as they already exist in settings.json

--- app/client/lib/bootstrap.js
+++ app/client/lib/bootstrap.js
@@ -1,7 +1,3 @@
-Accounts.ui.config({
-  passwordSignupFields: Meteor.settings.public.Accounts.config.passwordSignupFields
-});
-
 if (Meteor.isClient) {
 	Meteor.startup(function() {
 		getUserGeolocation();
