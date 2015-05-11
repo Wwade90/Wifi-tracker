ab9a7eaad9a16768b599740da1da2d76f5111082
Joe Burdick <josephdburdick@gmail.com>
2015-05-11 17:21:20 -0400

    Add string helpers.

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -2,9 +2,18 @@
 /* Client and Server Helpers */
 /*****************************************************************************/
 
+// String helpers
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 String.prototype.capitalizeFirstLetter = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+function toTitleCase(string){
+    return string.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+String.prototype.toTitleCase  = function() {
+    // return this.charAt(0).toUpperCase() + this.slice(1);
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
