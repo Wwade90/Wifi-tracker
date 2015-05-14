90c354719f2432e0de7a9954321e00b5a4821653
Joe <josephdburdick@gmail.com>
2015-05-13 20:51:51 -0400

    One infowindow for each pin :thumb:

--- app/client/templates/shared/nearby_map/nearby_map.js
+++ app/client/templates/shared/nearby_map/nearby_map.js
@@ -27,33 +27,39 @@ Template.NearbyMap.helpers({
 Template.NearbyMap.created = function () {
 	// getUserGeolocation();
   GoogleMaps.ready('map', function(map) {
-
-  	Venues.find().forEach(function(doc){
-  		var infowindow = new google.maps.InfoWindow({
-			    content: [
-			    	'<header>' + doc.location.name.capitalizeFirstLetter() + '</header>',
-			    	'<main>' + doc.location.address.capitalizeFirstLetter() + '</main>'].join('')
-			});
-			var marker = new google.maps.Marker({
+    var infowindow = new google.maps.InfoWindow();
+  	
+    Venues.find().forEach(function(doc){
+			
+      var marker = new google.maps.Marker({
 	      position: new google.maps.LatLng(doc.location.lat, doc.location.lon),
 	      map: map.instance
 	    });
+
 	    marker.set('title', doc.location.name);
 	    marker.addListener('click',function(){
         window.location.hash = '#t_' + doc._id;
+        infowindow.setContent(
+          [
+            '<header>' + doc.location.name.capitalizeFirstLetter() + '</header>',
+            '<main>' + doc.location.address.capitalizeFirstLetter() + '</main>'
+          ].join(''));
         infowindow.open(map.instance, marker);
       });
-   //    google.maps.event.addListener(marker, 'click', function() {
-			  
-			// });		
+      	
 		});
 
-		var marker = new google.maps.Marker({
+		var currentGeolocation = new google.maps.Marker({
       position: new google.maps.LatLng(Session.get('lat'), Session.get('lon')),
       icon: 'http://maps.gstatic.com/mapfiles/markers2/icon_green.png',
       map: map.instance,
       title: "Your Geolocation"
     });		
+
+    currentGeolocation.addListener('click',function(){
+      infowindow.setContent('<header>You are Here</header>');
+      infowindow.open(map.instance, currentGeolocation);
+    });
 		
 		$(window).on('resize', function(){
 			map.instance.setCenter(new google.maps.LatLng(Session.get('lat'), Session.get('lon')));
