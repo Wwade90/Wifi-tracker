7e4fe14d672acc7590c1e11f6e28836a0f8a6c84
Joe <josephdburdick@gmail.com>
2015-05-13 20:46:21 -0400

    Add toTitleCase helper.

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -8,3 +8,9 @@ function capitalizeFirstLetter(string) {
 String.prototype.capitalizeFirstLetter = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
+function toTitleCase(string){
+    return string.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+String.prototype.toTitleCase  = function() {
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
