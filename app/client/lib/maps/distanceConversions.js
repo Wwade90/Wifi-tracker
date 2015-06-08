70ab1273391b2dcd929d9691cbac725df4c705ec
Joe Burdick <josephdburdick@gmail.com>
2015-06-07 20:40:02 -0400

    Get a finer meter to mile conversion, only convert to feet when under .25 mi.

--- app/client/lib/maps/distanceConversions.js
+++ app/client/lib/maps/distanceConversions.js
@@ -5,13 +5,13 @@ Various distance conversions
 */
 
 toKM = function(distance){ return distance * 0.001 };
-toMi = function(distance){ return distance * 0.00062137 };
+toMi = function(distance){ return distance * 0.000621371 };
 toFt = function(distance){ return distance * 3.28084 };
 toBlocks = function(distance){ return distance * 0.012427423844746679 };
 calculateDistance = function(distance){
 	var distanceValue,
 			distanceUnit;
-	if (toMi(distance) <= 1){
+	if (toMi(distance) <= 0.25){
 		distanceValue = toFt(distance).to2Decimals();
 		distanceUnit = 'ft';
 	} else { 
