3a1ae092bcfad88878d8a6deb30d4dafc6323ab1
Joe Burdick <josephdburdick@gmail.com>
2015-06-08 00:39:55 -0400

    Properly setup home controller.

--- app/lib/controllers/home_controller.js
+++ app/lib/controllers/home_controller.js
@@ -1,14 +1,34 @@
-
-
-// Is the below subscription not necessary?
 HomeController = RouteController.extend({
-  layoutTemplate: 'MasterLayout',
+  template: 'Home',
 
-  subscriptions: function() {
-  	
+  // subscriptions: function() {
+  // },
+  waitOn: function(){
+  	return [
+  			Meteor.subscribe('distances'),
+  			Meteor.subscribe('nearestVenues', {
+					limit : Meteor.settings.public.Defaults.defaultVenueLimit,
+					coordinates : !!Session.get('currentUserCoords') ? Session.get('currentUserCoords') : Meteor.settings.public.Defaults.defaultUserCoords, 
+					distanceLimit : !!Session.get('currentDistanceLimit') ? Session.get('currentDistanceLimit') : Meteor.settings.public.Defaults.defaultDistanceLimit 
+	      })
+	  ]
   },
-
+  data: function(){
+  	return {
+  		distances: Distances.find(),
+  		venues: Venues.find(
+			{ 'coordinates': 
+				{
+					$near: !!Session.get('currentUserCoords') ? Session.get('currentUserCoords') : Meteor.settings.public.Defaults.defaultUserCoords,
+		  		$maxDistance: !!Session.get('currentDistanceLimit') ? Session.get('currentDistanceLimit') : Meteor.settings.public.Defaults.defaultDistanceLimit
+				}
+			})
+		}
+  },
   action: function() {
-    this.render('Home');
+    if (this.ready())
+    	this.render('VenueList');
+    else
+    	this.render('Loading');
   }
 });
