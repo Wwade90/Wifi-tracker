04e83833617ca18d264dd00e710fc5fa4b08b20f
Joe Burdick <josephdburdick@gmail.com>
2015-06-14 19:18:44 -0400

    Add location button.

---
+++ app/client/templates/shared/buttons/add_location_button/add_location_button.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* AddLocationButton: Event Handlers */
+/*****************************************************************************/
+Template.AddLocationButton.events({
+});
+
+/*****************************************************************************/
+/* AddLocationButton: Helpers */
+/*****************************************************************************/
+Template.AddLocationButton.helpers({
+});
+
+/*****************************************************************************/
+/* AddLocationButton: Lifecycle Hooks */
+/*****************************************************************************/
+Template.AddLocationButton.created = function () {
+};
+
+Template.AddLocationButton.rendered = function () {
+};
+
+Template.AddLocationButton.destroyed = function () {
+};
