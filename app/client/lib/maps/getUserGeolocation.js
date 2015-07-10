3ff80a159ac83ddbc971125c76d885ec68416ae1
Joe <josephdburdick@gmail.com>
2015-07-09 22:10:26 -0400

    Use promise to retrieve geolocation.

--- app/client/lib/maps/getUserGeolocation.js
+++ app/client/lib/maps/getUserGeolocation.js
@@ -2,28 +2,31 @@
 
 getUserGeolocation
 
-1. Grabs current position
+Uses a promise to:
+1. Grab current position
 2. Sets lat, lon, currentUserCoords
 3. Calls reverseGeocode server method
 
 */
 
+function getCurrentPositionDeferred(options) {
+  var deferred = $.Deferred();
+  navigator.geolocation.getCurrentPosition(deferred.resolve, deferred.reject, options);
+  return deferred.promise();
+};
+
 getUserGeolocation = function(){
-	if (Meteor.isClient) {
-		var locationData = 0,
-				userGeolocation;
-		navigator.geolocation.getCurrentPosition(function(position) {
+  if (Meteor.isClient) {
+    getCurrentPositionDeferred({
+      enableHighAccuracy: true
+    }).done(function(position) {
       Session.set('lat', position.coords.latitude);
       Session.set('lon', position.coords.longitude);
       Session.set('currentUserCoords', [position.coords.latitude, position.coords.longitude]);
-      console.log("Geolocation: ", [position.coords.latitude, position.coords.longitude]);
-			return position.coords;
-    },
-    function() {
-        alert('Position could not be determined.')
-    },
-    {
-        enableHighAccuracy: true
+      currentUserCoords = [position.coords.latitude, position.coords.longitude];
+      return currentUserCoords;
+    }).fail(function(error) {
+    }).always(function() {
     });
   }
-};
+}
