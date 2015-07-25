5feeb11b75197825446c2a1a2160ac834c58fa23
Joe Burdick <josephdburdick@gmail.com>
2015-07-24 23:36:40 -0400

    Add callback to modal shown event.

--- app/client/templates/shared/dialogs/add_location_dialog/add_location_dialog.js
+++ app/client/templates/shared/dialogs/add_location_dialog/add_location_dialog.js
@@ -2,15 +2,27 @@
 /* AddLocationDialog: Event Handlers */
 /*****************************************************************************/
 Template.AddLocationDialog.events({
-	// 'shown.bs.modal #AddLocationDialog': function(e){
-	// 	console.log('triggered dialog');
-	// }
+	'click [data-foursquare-id]': function(event, template){
+		Meteor.call('getVenue', event, function (error, result) {
+			if (!error){
+				console.log(result);
+			} else {
+				console.error(error);
+			}
+		});
+	},
+	'shown.bs.modal #AddLocationDialog': function(){
+		console.log('modal callback');
+	} 
 });
 
 /*****************************************************************************/
 /* AddLocationDialog: Helpers */
 /*****************************************************************************/
 Template.AddLocationDialog.helpers({
+	locations: function(){
+    return Template.instance().nearbyLocations.get();
+  }
 });
 
 /*****************************************************************************/
