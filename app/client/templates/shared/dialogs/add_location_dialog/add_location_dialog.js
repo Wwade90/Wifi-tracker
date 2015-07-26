4bb88cf78c020cd25de70fe42d6c9de6d23ed4bc
Joe Burdick <josephdburdick@gmail.com>
2015-07-26 19:04:26 -0400

    Add show and hidden callbacks for Add Location Dialog

--- app/client/templates/shared/dialogs/add_location_dialog/add_location_dialog.js
+++ app/client/templates/shared/dialogs/add_location_dialog/add_location_dialog.js
@@ -1,10 +1,19 @@
 /*****************************************************************************/
 /* AddLocationDialog: Event Handlers */
 /*****************************************************************************/
+var nearbyLocationsSub;
 Template.AddLocationDialog.events({
 	'show.bs.modal #AddLocationDialog': function(){
-		console.log('modal callback');
-	} 
+		if (nearbyLocationsSub)
+			nearbyLocationsSub.stop();
+		nearbyLocationsSub = Meteor.subscribe('nearbyLocations', {coords: !!Session.get('currentUserCoords') ? Session.get('currentUserCoords') : Meteor.settings.public.Defaults.defaultUserCoords})
+	},
+	'hidden.bs.modal #AddLocationDialog': function(event, template){
+		if (nearbyLocationsSub)
+			nearbyLocationsSub.stop();
+		template.find('form').reset();
+		tempalte.find('a[data-foursquare-id]').removeClass('active');
+	}
 });
 
 /*****************************************************************************/
