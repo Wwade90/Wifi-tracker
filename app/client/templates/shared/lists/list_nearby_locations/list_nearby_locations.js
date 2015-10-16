69ba072f17cad2733583da5ad42d6df9357917f9
Joe Burdick <josephdburdick@gmail.com>
2015-10-16 09:03:48 -0400

    Progress on add-location view.

--- app/client/templates/shared/lists/list_nearby_locations/list_nearby_locations.js
+++ app/client/templates/shared/lists/list_nearby_locations/list_nearby_locations.js
@@ -1,24 +1,33 @@
+/* JSHint Template: false */
+/* JSHint TAPi18n: false */
+/* JSHint calculateDistance: false */
 /*****************************************************************************/
 /* ListNearbyLocations: Event Handlers */
 /*****************************************************************************/
 var resetForm = function(form){
-
+  console.log('Resetting this form!');
+  console.log(form);
 };
-var autofillForm = function(container, object){
-  var form = container.find('form');
-     /*
-      • name
-      • categories
-      • tags
-      • address
-    */
+var autofillForm = function(container, response){
+   /*
+    • name
+    • categories
+    • tags
+    • address
+  */
+  var $form = container.find('form');
   var venueData = {
-    name: object.data.name,
-    formattedAddress : object.data.location.formattedAddress.join(', '),
-    latitude: object.data.location.lat,
-    longitude: object.data.location.lng
-  }
-  form
+      name: response.data.name,
+      $formattedAddress : response.data.location.formattedAddress.join(', '),
+      latitude: response.data.location.lat,
+      longitude: response.data.location.lng,
+      categories: _.pluck(response.data.categories, 'name')
+    };
+    // categoriesTemplate = [
+    //
+    // ];
+
+  $form
     .find('#location_name').val(venueData.name)
       .prop('disabled', true).end()
     .find('#location_address').val(venueData.formattedAddress)
@@ -26,16 +35,18 @@ var autofillForm = function(container, object){
     .find('#location_latitude').val(venueData.latitude)
       .prop('disabled', true).end()
     .find('#location_longitude').val(venueData.longitude)
-      .prop('disabled', true);
+      .prop('disabled', true).end()
+    .find('#location_categories').val(venueData.categories);
 
-  if (!form.find('.alert').length){
+  if (!$form.find('.alert').length){
     var alertTemplate = [
       '<div class="alert alert-info alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="alert-body"></div></div>'
     ].join('\n');
-    form.append(alertTemplate)
-    var alertEl = form.find('.alert'),
+    $form.append(alertTemplate);
+    var alertEl = $form.find('.alert'),
         alertBody = alertEl.find('.alert-body');
-    alertBody.text("This is just a test");
+
+    alertBody.text('This is just a test');
   }
 
 };
@@ -46,18 +57,19 @@ Template.ListNearbyLocations.events({
     // Show the add venue form tab
     $('[href="#tab--form-addVenue"]').tab('show');
     $('#network_create').trigger('reset');
-    if (event.currentTarget.dataset.function.indexOf('resetForm') > -1){
-    // If link has data-attribute to reset form, make it so
-      resetForm(form);
+
+    if ($(event.currentTarget).data('trigger').indexOf('resetForm') > -1){
+      // If link has data-attribute to reset form, make it so
+      resetForm($form);
     }
   },
   'click a[role="tab"]': function(event){
     event.preventDefault();
     $(event.currentTarget).tab('show');
   },
-  'click [data-foursquare-id]': function(event, template){
+  'click [data-foursquare-id]': function(event){
     var venueId = event.currentTarget.dataset.foursquareId;
-    var getVenue = Meteor.call('getVenue', venueId, function(error, response){
+    Meteor.call('getVenue', venueId, function(error, response){
       if (!error){
         var foursquareVenue = response.data.response.venue;
         $('[href="#tab--form-addVenue"]').tab('show');
@@ -66,7 +78,7 @@ Template.ListNearbyLocations.events({
       }
       else {
         console.error(error.message);
-        alert("Could not retrieve response from server. Reason: " + error.message);
+        alert('Could not retrieve response from server. Reason: ' + error.message);
       }
     });
   }
